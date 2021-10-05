5db0dfc913e0ec4d95e954094daa88030e9ebca0
Rinaldas <rinaldas.daujotas@gmail.com>
2021-10-05 10:40:29 +0300

    home activity

--- app/src/main/java/com/example/app_dev_money_tracking/Home_activity.java
+++ app/src/main/java/com/example/app_dev_money_tracking/Home_activity.java
@@ -2,15 +2,17 @@ package com.example.app_dev_money_tracking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class Home extends AppCompatActivity
+public class Home_activity extends AppCompatActivity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+//        getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
     }
 }
--- app/src/main/java/com/example/app_dev_money_tracking/MainActivity.java
+++ app/src/main/java/com/example/app_dev_money_tracking/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.app_dev_money_tracking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(MainActivity.this, Home_activity.class));
 
         //test
     }
